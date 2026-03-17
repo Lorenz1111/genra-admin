87bc7c60a1d2d05df3aeed7eb0fb3132f971a407
Lorenz1111 <gilbuena.johnlaurenced@gmail.com>
2026-03-18 01:24:50 +0800

    fix base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const isGithubPages = process.env.GITHUB_ACTIONS || process.env.NODE_ENV === 'production' && process.env.VITE_DEPLOY_TARGET === 'gh-pages'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()], 
-  base: '/genra-admin/', // Para sa GitHub Pages deployment
+  base: isGithubPages ? '/genra/' : '/',
 })
